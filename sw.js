6417d6bae270ab19c82c161b306c804c1cff6582
terokulju95-boop <terokulju95@gmail.com>
2026-06-15 13:55:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // tarvitsee koskea. Kasvata VERSIONia vain jos haluat pakottaa kaiken
 // uudelleenlatauksen (esim. ikonit tai tiedostolista muuttuivat).
 
-const VERSION = 'v11';
+const VERSION = 'v12';
 const CACHE   = 'kulju-' + VERSION;
 
 // Suhteelliset polut – toimivat sekä juuressa että alipolussa (GitHub Pages).
